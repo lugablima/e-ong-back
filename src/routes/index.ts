5f36f0f6f682db80c39156e57a35d4b967c3e374
Lucas <lugablima@gmail.com>
2022-09-29 11:19:07 -0300

    feat: add file that centralizes all application routes

---
+++ src/routes/index.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+// import authRouter from "./authRouter";
+// import validateToken from "../middlewares/tokenValidatorMiddleware";
+// import testsRouter from "./testsRouter";
+
+const router: Router = Router();
+
+// router.use(authRouter);
+// router.use(validateToken);
+// router.use(testsRouter);
+
+export default router;
